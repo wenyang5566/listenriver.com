0e75a5417aaf1cdf31e637935ed5fafc22d19148
Wen-Yang Lin <gohugo0223@gmail.com>
2026-04-10 22:23:47 +0800

    Fix Hugo server routing and internal content links

--- scripts/cleanup-generated.ps1
+++ scripts/cleanup-generated.ps1
@@ -9,8 +9,26 @@ $targets = @(
   ".tmp_hugo_check",
   ".tmp_hugo_cache",
   ".tmp_hugo_profile",
+  ".tmp_hugo_repro",
+  ".tmp_hugo_verify",
+  ".tmp_layout_probe",
+  ".tmp_layout_probe2",
   "public-build-check",
-  "hugo-build.log"
+  "hugo-build.log",
+  "generated-paths.txt",
+  ".tmp_probe_lang.toml",
+  ".tmp_probe_more.toml",
+  ".tmp_probe_outputs.toml",
+  ".tmp_server_probe.toml",
+  ".tmp_server_probe2.toml",
+  ".tmp_server_probe3.toml",
+  ".tmp_server_probe4.toml",
+  "hugo-repro.err.log",
+  "hugo-repro.out.log",
+  "hugo-server.err.log",
+  "hugo-server.out.log",
+  "hugo1313.err.log",
+  "hugo1313.out.log"
 )
 
 foreach ($relativePath in $targets) {
